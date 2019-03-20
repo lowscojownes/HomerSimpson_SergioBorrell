6fc30560c3ffa926e970d44d337cbfc84fa4f217
LoWs-TOP <sergio.borrell.flores@gmail.com>
2019-03-20 11:07:11 +0100

    Animación del titulo hecha.

--- app/src/main/java/com/example/homersimpson_sergioborrell/MainActivity.java
+++ app/src/main/java/com/example/homersimpson_sergioborrell/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.homersimpson_sergioborrell;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Typecasting the Image View
+        ImageView view = (ImageView) findViewById(R.id.simpsonstitle);
+
+        // Setting animation_list.xml as the background of the image view
+        view.setBackgroundResource(R.drawable.thesimpsons_animation);
+
+        // Typecasting the Animation Drawable
+        AnimationDrawable frameAnimation = (AnimationDrawable) view.getBackground();
+        frameAnimation.start();
     }
+
 }
